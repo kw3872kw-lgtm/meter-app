99725d4f0cdb56751c2c5df4222662bb98c7aadc
kw3872kw-lgtm <kw3872kw@gmail.com>
2026-06-25 21:05:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meter-v2-cache-v1';
+const CACHE_NAME = 'meter-v2-cache-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
